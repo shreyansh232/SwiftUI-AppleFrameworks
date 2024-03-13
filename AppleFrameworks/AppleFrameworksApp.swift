736ad0020b3b04dce4d68e54879a5527e738420a
shreyansh232 <shreyansh487@gmail.com>
2024-03-13 22:12:29 +0530

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Shreyansh Pandey on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
