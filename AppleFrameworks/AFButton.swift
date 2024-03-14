02f3172b3be80ec950fedfbd8515a2ec4279ad22
shreyansh232 <shreyansh487@gmail.com>
2024-03-15 01:40:50 +0530

    Changes

---
+++ AppleFrameworks/AFButton.swift
@@ -0,0 +1,28 @@
+//
+//  AFButton.swift
+//  AppleFrameworks
+//
+//  Created by Shreyansh Pandey on 15/03/24.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    
+    var title: String
+    var body: some View {
+        Text(title)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .frame(width: 280, height: 50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
+
+struct AFButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AFButton(title: "Test title")
+    }
+}
--- AppleFrameworks/FrameworkDetailView.swift
+++ AppleFrameworks/FrameworkDetailView.swift
@@ -11,8 +11,38 @@ struct FrameworkDetailView: View {
     
     var framework: Framework;
     var body: some View {
+        
         VStack{
+            HStack{
+                Spacer()
+                Button{
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label)) //color depends on the label
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .padding()
+            
+            
+            Spacer()
             FrameworkTitleView(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button(){
+                
+            } label: {
+                AFButton(title: "Learn More")
+                
+                
+            }
+            
         }
     }
 }
