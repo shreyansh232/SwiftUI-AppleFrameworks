812893614f08ce3a9d0d85bd339064594331363f
shreyansh232 <shreyansh487@gmail.com>
2024-03-15 00:23:12 +0530

    Changes

--- AppleFrameworks/FrameworkGridView.swift
+++ AppleFrameworks/FrameworkGridView.swift
@@ -8,19 +8,18 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()), //2 columns
+                               GridItem(.flexible())]
     var body: some View {
-        VStack{
-        Image("app-clip")
-                .resizable()
-                .frame(width: 90, height: 90)
-            Text("App Clips")
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit()
-                .minimumScaleFactor(0.6)
-            
-            
-            
+        
+        LazyVGrid(columns: columns){
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip");
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip");
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip");
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip");
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip");
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip");
             
             
         }
@@ -31,4 +30,26 @@ struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
     }
+}
+
+
+
+struct FrameworkTitleView: View {
+    let name: String;
+    let imageName: String;
+    var body: some View{
+        VStack{
+        Image(imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        
+    }
+  
+    
 }
